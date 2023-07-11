19fd09d7c689cc1a2edc869378aad3becb51aed3
Nah0012 <s2110905@s.tsukuba.ac.jp>
2023-07-12 03:21:42 +0900

    md実装中

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import preact from "@astrojs/preact";
+import mdx from "@astrojs/mdx";
+
+import markdoc from "@astrojs/markdoc";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), preact()]
+  integrations: [react(), tailwind(), preact(), mdx(), markdoc()]
 });
